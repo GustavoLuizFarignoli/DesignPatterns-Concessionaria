98f028cc7b2895f85971d7b3825ffbe1c86dd8cb
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2023-10-09 17:09:04 -0300

    melhora de menu de sessão

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,77 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int op = 0;
         Scanner teclado = new Scanner(System.in);
+        boolean running = true;
 
-        while(op != 5){
-            System.out.println("1-Login\n2-Verificar Login\n3-Logout\n4-Fazer Cadastro\n5-Sair\n");
-            op = teclado.nextInt();
-            switch(op){
-                case 1:
-                    System.out.println("Digite o seu email: ");
-                    teclado.nextLine();
-                    String email = teclado.nextLine();
-                    System.out.println("Digite sua senha: ");
-                    String senha = teclado.nextLine();
-                    LoginFacade.logar(email,senha);
-                    break;
-                case 2:
-                    if(LoginSingleton.instancia != null) {
-                        System.out.println(LoginSingleton.instancia);
-                    } else{
-                        System.out.println("Usuário não logado!");
-                    }
-                    break;
-                case 3:
-                    LoginSingleton.logout();
-                    break;
-                case 4:
-                    System.out.println("Digite o seu email: ");
-                    teclado.nextLine();
-                    String emailcad = teclado.nextLine();
-                    System.out.println("Digite sua senha: ");
-                    String senhacad = teclado.nextLine();
-                    LoginFacade.cadastrar(emailcad,senhacad);
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    LoginFacade.debug();
-                    break;
+        while(running) {
+            if (LoginSingleton.instancia == null) {
+                running = loginmenu(teclado);
+            } else {
+                menu(teclado);
             }
         }
+    }
 
+    public static boolean loginmenu(Scanner teclado){
+        int op = 0;
+        boolean running = true;
+        System.out.println("1-Login\n2-Verificar Login\n3-Fazer Cadastro\n4-Sair");
+        op = teclado.nextInt();
+        switch(op) {
+            case 1:
+                System.out.println("Digite o seu email: ");
+                teclado.nextLine();
+                String email = teclado.nextLine();
+                System.out.println("Digite sua senha: ");
+                String senha = teclado.nextLine();
+                LoginFacade.logar(email, senha);
+                break;
+            case 2:
+                if (LoginSingleton.instancia != null) {
+                    System.out.println(LoginSingleton.instancia);
+                } else {
+                    System.out.println("Usuário não logado!");
+                }
+                break;
+            case 3:
+                System.out.println("Digite o seu email: ");
+                teclado.nextLine();
+                String emailcad = teclado.nextLine();
+                System.out.println("Digite sua senha: ");
+                String senhacad = teclado.nextLine();
+                LoginFacade.cadastrar(emailcad, senhacad);
+                break;
+            case 4:
+                running = false;
+                break;
+            case 5:
+                LoginFacade.debug();
+                break;
+        }
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        return running;
+    }
+
+    public static void menu (Scanner teclado){
+        int op = 0;
+        System.out.println("1-Ver Estoque\n2-Realizar Compra\n3-Realizar Aluguel\n4-Fazer Logout");
+        op = teclado.nextInt();
+        switch(op) {
+            case 1:
+                System.out.println("Estoque");
+                break;
+            case 2:
+                System.out.println("Compra");
+                break;
+            case 3:
+                System.out.println("Aluguel");
+                break;
+            case 4:
+                System.out.println("Realizando log out...");
+                LoginSingleton.logout();
+                break;
+        }
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
     }
 }
